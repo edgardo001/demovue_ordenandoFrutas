d6201bd0aa20b333aceb997a8d5f965eae3d64a8
Edgardo vasquez <edgardo.a.v@gmail.com>
2019-05-08 11:00:33 -0400

    Creando componentes vue

--- src/components/Cabecera.vue
+++ src/components/Cabecera.vue
@@ -1,5 +1,7 @@
 <template>
-    <div>
+    <div class="text-center">
+        <!--El "@" indica que se debe volver a la raiz y desde alli partir buscando el directorio-->
+        <img alt="Vue logo" src="@/assets/logo.png">
         <h1>Frutas</h1>
     </div>
 </template>
---
+++ src/components/Lista.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <ul class="list-group">
+            <!--Si pinta en rojo la linea, se debe agregar el elemento ":key"-->
+            <li v-for="(item) of arrayOrdenado" :key="item.id" class="list-group-item d-flex justify-content-between align-items-center" :class="item.clase" @click="aumentar(item.id)">
+                {{item.id}} - {{item.nombre}}
+                <span class="badge badge-primary badge-pill">{{item.cantidad}}</span>
+            </li>
+            <button class="btn btn-danger btn-bloc" @click="reiniciar">Reiniciar</button>
+        </ul>
+    </div>
+</template>
+
+<script>
+
+import { mapState, mapMutations } from 'vuex'
+
+export default {
+  name: 'Lista',
+  computed: {
+    // Los "state" del "store.js" siempre se pasan entre "[]"
+    ...mapState(['frutas']),
+    arrayOrdenado () {
+      // Se debe usar slice antes del sort, no se puede mutar el objeto original
+      // https://github.com/vuejs/eslint-plugin-vue/blob/master/docs/rules/no-side-effects-in-computed-properties.md
+      console.log(this.frutas)
+      return this.frutas.sort((a, b) => b.cantidad - a.cantidad)
+    }
+  },
+  methods: {
+    // Las "mutations" del "store.js" siempre se pasan entre "[]"
+    ...mapMutations(['aumentar', 'reiniciar'])
+  }
+}
+</script>
